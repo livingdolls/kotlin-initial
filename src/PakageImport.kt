5fb90bd88b1bdc1a58738a01540f56c9070e9afb
livingdolls <nanangyorozuya@gmail.com>
2026-03-21 08:37:16 +0700

    import

---
+++ src/PakageImport.kt
@@ -0,0 +1,7 @@
+import com.yurina.kot.util.sayHello
+import com.yurina.kot.util.sayGoodbya
+
+fun main() {
+    sayHello("Setiawan")
+    sayGoodbya("Yurina")
+}
---
+++ src/com/yurina/kot/util/StringUtil.kt
@@ -0,0 +1,9 @@
+package com.yurina.kot.util
+
+fun sayHello(name: String) {
+    println("Hello $name!")
+}
+
+fun sayGoodbya(name: String) {
+    println("Goodbya $name!")
+}
